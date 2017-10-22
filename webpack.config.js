1a756900adc2bd8a925f08cf087ef24c1609b00c
Jordan Santell <jordan@jsantell.com>
2017-10-22 14:55:02 -0700

    Add tween and dat

--- webpack.config.js
+++ webpack.config.js
@@ -51,6 +51,13 @@ module.exports = {
       amd: 'three',
       root: 'THREE',
     },
+    tween: {
+      commonjs: 'tween',
+      commonjs2: 'tween',
+      amd: 'tween',
+      root: 'TWEEN',
+    },
+    'dat.gui': 'dat',
   },
   resolve: {
     extensions: ['.js']
